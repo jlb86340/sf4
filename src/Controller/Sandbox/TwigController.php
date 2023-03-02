d8426a8b9c8e9277427ca9b13a6cdac2969b2ca8
mathieu <math86340@orange.fr>
2023-03-02 11:20:52 +0100

    2.1

---
+++ src/Controller/Sandbox/TwigController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Sandbox;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sandbox/twig', name: 'sandbox_twig')]
+class TwigController extends AbstractController
+{
+}
+
+
