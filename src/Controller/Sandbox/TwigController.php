35cecdf974be2fa453131856a684044f989f0dc8
mathieu <math86340@orange.fr>
2023-03-02 11:53:04 +0100

    2.4 et 2.5

--- src/Controller/Sandbox/TwigController.php
+++ src/Controller/Sandbox/TwigController.php
@@ -9,6 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sandbox/twig', name: 'sandbox_twig')]
 class TwigController extends AbstractController
 {
+    #[Route('/vue1', name: '_vue1')]
+    public function vue1Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue1.html.twig');
+    }
+    #[Route('/vue2', name: '_vue2')]
+    public function vue2Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue2.html.twig');
+    }
+
 }
-
-
