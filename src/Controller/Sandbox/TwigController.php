fade42ecefba1f572ae44c1aa5eb66030e1f13d2
mathieu <math86340@orange.fr>
2023-03-07 08:47:37 +0100

    4

--- src/Controller/Sandbox/TwigController.php
+++ src/Controller/Sandbox/TwigController.php
@@ -9,27 +9,59 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sandbox/twig', name: 'sandbox_twig')]
 class TwigController extends AbstractController
 {
-    #[Route('/vue1', name: '_vue1')]
+    #[Route(
+        '/vue1',
+        name: '_vue1'
+    )]
     public function vue1Action(): Response
     {
         return $this->render('Sandbox/Twig/vue1.html.twig');
     }
-    #[Route('/vue2', name: '_vue2')]
+
+    #[Route(
+        '/vue2',
+        name: '_vue2'
+    )]
     public function vue2Action(): Response
     {
         return $this->render('Sandbox/Twig/vue2.html.twig');
     }
 
-    #[Route('/vue3', name: '_vue3')]
+    #[Route(
+        '/vue3',
+        name: '_vue3'
+    )]
     public function vue3Action(): Response
     {
         return $this->render('Sandbox/Twig/vue3.html.twig');
     }
 
-    #[Route('/vue4', name: '_vue4')]
+    #[Route(
+        '/vue4',
+        name: '_vue4'
+    )]
     public function vue4Action(): Response
     {
         return $this->render('Sandbox/Twig/vue4.html.twig');
     }
 
+    // surtout pas d'attribut Route
+    // note : cette action devrait être dans un contrôleur dédié
+    public function palmaresAction(int $n): Response
+    {
+        // normalement devrait être extrait de la base de données
+        $produits = [];
+        for ($i = 1; $i <= $n; $i ++)
+            $produits[] = ['denomination' => 'produit'.$i, 'prix' => 1000-$i];
+        return $this->render('Sandbox/Layouts/palmares.html.twig', [ 'produits' => $produits ]);
+    }
+
+    #[Route(
+        '/vue5',
+        name: '_vue5'
+    )]
+    public function vue5Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue5.html.twig');
+    }
 }
